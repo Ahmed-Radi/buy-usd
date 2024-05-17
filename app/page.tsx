16beffa13b8fc2a6bc04d292d37d0669539f8001
Ahmed Radi <52893501+Ahmed-Radi@users.noreply.github.com>
2024-05-17 15:25:29 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,12 +29,12 @@ export default function Home() {
   useEffect(() => {
     const result: number = egy / 45
     const addPercentage: number = result + (result * 0.1)
-    const totalEGP: number = addPercentage * 52
+    const totalEGP: number = addPercentage * 50
     setTotalEgy(totalEGP)
   }, [egy])
 
   useEffect(() => {
-    const result: number = usd * 52
+    const result: number = usd * 50
     const addPercentage: number = result + (result * 0.1)
     const totalUSD: number = addPercentage
     setTotalUsd(totalUSD)
